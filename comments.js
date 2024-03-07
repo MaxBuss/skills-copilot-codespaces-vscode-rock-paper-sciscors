ef5c646f02669ae983f7552774090f46853aa81c
MaxBuss <99990682+MaxBuss@users.noreply.github.com>
2024-03-07 21:34:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// create a web server
+// create a route that accepts a POST request to /comments
+// create a middleware that will parse the incoming request body
+// create a middleware that will log the incoming request body
+// create a middleware that will send a response to the client
+// start the server on port 3000
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/comments', (req, res) => {
+  console.log(req.body);
+  res.send('POST request sent to /comments');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
